03c7d5d62a5245f960f635b33aea80b88e18e439
amhayslip <amhayslip@gmail.com>
2013-12-04 10:57:14 -0600

    added dummy database for users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@
 class PagesController < ApplicationController
 
 	def home
+		@users = User.all
+		@user1 = User.find(1)
+		@user2 = User.find(2)
+		@user3 = User.find(3)
+		@user4 = User.find(4)
 	end
 
 
